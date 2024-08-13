0e8ca8ccbc7b0564e8fe83a14c066bd3163976ab
Jammykidnow <132238615+Jammykidnow@users.noreply.github.com>
2024-08-12 20:36:51 -0400

    Update index.js

--- Bot-ghost-status-remover-by-GlaceYT-main/index.js
+++ Bot-ghost-status-remover-by-GlaceYT-main/index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Tennessee State Roleplay Utilities bot","#TennesseeRPonTOP"];
 
 
 let currentIndex = 0;
